aaf08eb6c2b197676fa945d04c857fe8977c8b1b
Gavin Bauman <gavinbauman1322@gmail.com>
2023-12-09 16:01:43 -0500

    update paths with placeholders

--- src/mediaScannerMachine.ts
+++ src/mediaScannerMachine.ts
@@ -5,8 +5,8 @@ export const mediaScannerMachine = createMachine(
   {
     /** @xstate-layout N4IgpgJg5mDOIC5QFtIEsCGBlAxhgdvmAE4B0aEANmAMRYAqAggEr0D6WAwowHIDaABgC6iUAAcA9rDQAXNBPyiQAD0QBGAMwBWLaQAcANgDsAJjVaBJgQYF6ANCACeiDQBYBpIxYEa1agVr+rgZaAL6hDqgQmLgERGSxhGj4UDQQCmDk+ABuEgDWmbB4+AAyaABGxBjEjoIiSCCS0nIKSqoIagCcep2enQadAv0aJl4hDs4IJoOkNsZaJjpG7q7hkejYxfGkifjJqSTEEmRilBgyAGbHyKRFBGWV1bXCSk2y8ooN7V09fQNDBhGYy0E0QgV6tg0GgM-gMBlc3VWERAURiWxIpE4AAswDg8vsAGJoagABRIyDQsGkClgaQyWVyBVIOBxeKJpPJlOp+FgdVeUnerS+iBMcI0pDcGj01iM5gWRlBUx0pFc7lsBjMlnMriMaxRG12GOxuPxKXZYDJxApVI+tMOx1Ip3OVytzNZeXNlut3N5Lwabxan1A7VFgIlrilMrlo0Vbj0+k6Iy0RkTrj0RhsGj1qM2cQxzDAkmIchSAFFiEdiLTmKWGCx6Hz-QLA20RUZlqRRtpLNCdMnFYD41Dh8sYdNutmDeiyKXshhKABXc6E4lwOlEBn5TJgOeL5dm1e++riZsfVtTIwCVykNTprSZnxeexORB6a+wrzacx6KUaTqT6Jc0IDEAFkJGyFdqDtCsHSdS5rlIZBwLAc0j35Zoz2FBAdXFLRfFFaENAzOFOkVDMPC0H98J-NQTCTAC0TzMgwIgg8oPXTJkkZTIkOyFDD0bE8MKFYNXw0DwRh8UVNGIgxn0mbt9GHBE9BMaY9EopFkXwCQIDgJQc0NYh0MFIMVEQABaAxFQs3ROns-otAjDN-ChMJkUM6dyCoMATJbLDaOMCVjDUlNXGmeZYwRCVhzMXx4QjQEGKA7Zdn2PzMNEjpAgMUgtETdtZXs1x+xfC9r28AQBFot9Fk6UZkqMzF3Ugi1ORtGkMpE8ypjUWVwyI+qnIzLtFTU3LhyhAq9DUOS4UaryCyLEsoHLSt4CbYSzJDTo1GvTpiPE0wjvkxAdXjHwoWqgRR0CK8FqY0hZ3nJcVtQrrtpFXb9sOm66Ju06EBmvKqq1SjDCMGb3PWQCmpY1qNqE0zz3CxVOmvKi3AK6wpQncJQiAA */
     context: {
-      basePath: "/Volumes/media/Movies",
-      destinationPath: "/Volumes/media/4KMovies",
+      basePath: "your source path here",
+      destinationPath: "your destination path here",
       directoriesToCheck: [],
       dirsToEvaluate: [],
       dirsToMove: [],
