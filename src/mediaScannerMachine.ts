f98161179da610036a797171a24fe7e8e3d40fcf
Gavin Bauman <gavinbauman1322@gmail.com>
2023-12-09 00:44:03 -0500

    remove unnecessary transitions bro

--- src/mediaScannerMachine.ts
+++ src/mediaScannerMachine.ts
@@ -3,6 +3,7 @@ import { checkFilePermissions, evaluateFiles, moveFiles, scanDirectories } from 
 
 export const mediaScannerMachine = createMachine(
   {
+    /** @xstate-layout N4IgpgJg5mDOIC5QFtIEsCGBlAxhgdvmAE4B0aEANmAMRYAqAggEr0D6WAwowHIDaABgC6iUAAcA9rDQAXNBPyiQAD0QBGAMwBWLaQAcANgDsAJjVaBJgQYF6ANCACeiDQBYBpIxYEa1agVr+rgZaAL6hDqgQmLgERGSxhGj4UDQQCmDk+ABuEgDWmbB4+AAyaABGxBjEjoIiSCCS0nIKSqoIagCcep2enQadAv0aJl4hDs4IJoOkNsZaJjpG7q7hkejYxfGkifjJqSTEEmRilBgyAGbHyKRFBGWV1bXCSk2y8ooN7V09fQNDBhGYy0E0QgV6tg0GgM-gMBlc3VWERAURiWxIpE4AAswDg8vsAGJoagABRIyDQsGkClgaQyWVyBVIOBxeKJpPJlOp+FgdVeUnerS+iBMcI0pDcGj01iM5gWRlBUx0pFc7lsBjMlnMriMaxRG12GOxuPxKXZYDJxApVI+tMOx1Ip3OVytzNZeXNlut3N5Lwabxan1A7VFgIlrilMrlo0Vbj0+k6Iy0RkTrj0RhsGj1qM2cQxzDAkmIchSAFFiEdiLTmKWGCx6Hz-QLA20RUZlqRRtpLNCdMnFYD41Dh8sYdNutmDeiyKXshhKABXc6E4lwOlEBn5TJgOeL5dm1e++riZsfVtTIwCVykNTprSZnxeexORB6a+wrzacx6KUaTqT6Jc0IDEAFkJGyFdqDtCsHSdS5rlIZBwLAc0j35Zoz2FBAdXFLRfFFaENAzOFOkVDMPC0H98J-NQTCTAC0TzMgwIgg8oPXTJkkZTIkOyFDD0bE8MKFYNXw0DwRh8UVNGIgxn0mbt9GHBE9BMaY9EopFkXwCQIDgJQc0NYh0MFIMVEQABaAxFQs3ROns-otAjDN-ChMJkUM6dyCoMATJbLDaOMCVjDUlNXGmeZYwRCVhzMXx4QjQEGKA7Zdn2PzMNEjpAgMUgtETdtZXs1x+xfC9r28AQBFot9Fk6UZkqMzF3Ugi1ORtGkMpE8ypjUWVwyI+qnIzLtFTU3LhyhAq9DUOS4UaryCyLEsoHLSt4CbYSzJDTo1GvTpiPE0wjvkxAdXjHwoWqgRR0CK8FqY0hZ3nJcVtQrrtpFXb9sOm66Ju06EBmvKqq1SjDCMGb3PWQCmpY1qNqE0zz3CxVOmvKi3AK6wpQncJQiAA */
     context: {
       basePath: "/Volumes/media/Movies",
       destinationPath: "/Volumes/media/4KMovies",
@@ -24,6 +25,7 @@ export const mediaScannerMachine = createMachine(
           },
         },
       },
+
       Scanning: {
         description:
           'Scan the media library and check for directories \n\nFor every file we can confirm is a directory, we add it to the context. \n\nIgnore the files already present in the ledger. Those are "known good"',
@@ -44,6 +46,7 @@ export const mediaScannerMachine = createMachine(
           ],
         },
       },
+
       CheckingFilePermissions: {
         description:
           "check the file permissions for all the files we need to scan.\n\nif we do not have read/write permissions, we update the context with the filenames/locations.\n\nif there are no files with read/write permissions, we move to the error state",
@@ -74,6 +77,7 @@ export const mediaScannerMachine = createMachine(
           ],
         },
       },
+
       ReportingErrors: {
         description:
           "Send a message with error details to the proper destination.\n\nErrors could be the lack of read/write permissions or path not existing",
@@ -86,6 +90,7 @@ export const mediaScannerMachine = createMachine(
           },
         },
       },
+
       EvaluatingFiles: {
         description:
           "Evaluate the files to determine their resolution. If they are 4K, move them to a new directory",
@@ -105,41 +110,23 @@ export const mediaScannerMachine = createMachine(
           ],
         },
       },
+
       MovingFiles: {
         description: "Move all the files present in context to the destination library",
         invoke: {
           input: ({context: { dirsToMove, destinationPath }}) => ({ dirsToMove, destinationPath }),
           src: fromPromise(({input: { dirsToMove, destinationPath }}) => moveFiles(dirsToMove, destinationPath)),
           id: "moveFiles",
-          onDone: [
-            {
-              target: "UpdateLedger",
-            },
-          ],
+
           onError: [
             {
               target: "ReportingErrors",
             },
           ],
+
+          onDone: "idle"
         },
-      },
-      UpdateLedger: {
-        description:
-          "Send a message with a list of all files moved to the proper destination.\n\nUpdate the ledger with the contents of the source library and save it to disk",
-        entry: [
-          {
-            type: "updateLedger",
-          },
-          {
-            type: "emailResults",
-          },
-        ],
-        on: {
-          RESTART: {
-            target: "idle",
-          },
-        },
-      },
+      }
     },
     types: {
       events: {} as { type: "START_SCAN" } | { type: "RESTART" },
@@ -158,9 +145,6 @@ export const mediaScannerMachine = createMachine(
   },
   {
     actions: {
-      emailErrors: ({ context, event }) => {},
-      updateLedger: ({ context, event }) => {},
-      emailResults: ({ context, event }) => {},
     },
     guards: {},
     delays: {},
