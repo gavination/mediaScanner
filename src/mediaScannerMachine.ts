9d13f48b0d35c38745cb36c09b5948882170e2fa
Gavin Bauman <gavinbauman1322@gmail.com>
2023-12-09 17:14:44 -0500

    add async/await keywords to fromPromise hooks

--- src/mediaScannerMachine.ts
+++ src/mediaScannerMachine.ts
@@ -5,8 +5,8 @@ export const mediaScannerMachine = createMachine(
   {
     /** @xstate-layout N4IgpgJg5mDOIC5QFtIEsCGBlAxhgdvmAE4B0aEANmAMRYAqAggEr0D6WAwowHIDaABgC6iUAAcA9rDQAXNBPyiQAD0QBGAMwBWLaQAcANgDsAJjVaBJgQYF6ANCACeiDQBYBpIxYEa1agVr+rgZaAL6hDqgQmLgERGSxhGj4UDQQCmDk+ABuEgDWmbB4+AAyaABGxBjEjoIiSCCS0nIKSqoIagCcep2enQadAv0aJl4hDs4IJoOkNsZaJjpG7q7hkejYxfGkifjJqSTEEmRilBgyAGbHyKRFBGWV1bXCSk2y8ooN7V09fQNDBhGYy0E0QgV6tg0GgM-gMBlc3VWERAURiWxIpE4AAswDg8vsAGJoagABRIyDQsGkClgaQyWVyBVIOBxeKJpPJlOp+FgdVeUnerS+iBMcI0pDcGj01iM5gWRlBUx0pFc7lsBjMlnMriMaxRG12GOxuPxKXZYDJxApVI+tMOx1Ip3OVytzNZeXNlut3N5Lwabxan1A7VFgIlrilMrlo0Vbj0+k6Iy0RkTrj0RhsGj1qM2cQxzDAkmIchSAFFiEdiLTmKWGCx6Hz-QLA20RUZlqRRtpLNCdMnFYD41Dh8sYdNutmDeiyKXshhKABXc6E4lwOlEBn5TJgOeL5dm1e++riZsfVtTIwCVykNTprSZnxeexORB6a+wrzacx6KUaTqT6Jc0IDEAFkJGyFdqDtCsHSdS5rlIZBwLAc0j35Zoz2FBAdXFLRfFFaENAzOFOkVDMPC0H98J-NQTCTAC0TzMgwIgg8oPXTJkkZTIkOyFDD0bE8MKFYNXw0DwRh8UVNGIgxn0mbt9GHBE9BMaY9EopFkXwCQIDgJQc0NYh0MFIMVEQABaAxFQs3ROns-otAjDN-ChMJkUM6dyCoMATJbLDaOMCVjDUlNXGmeZYwRCVhzMXx4QjQEGKA7Zdn2PzMNEjpAgMUgtETdtZXs1x+xfC9r28AQBFot9Fk6UZkqMzF3Ugi1ORtGkMpE8ypjUWVwyI+qnIzLtFTU3LhyhAq9DUOS4UaryCyLEsoHLSt4CbYSzJDTo1GvTpiPE0wjvkxAdXjHwoWqgRR0CK8FqY0hZ3nJcVtQrrtpFXb9sOm66Ju06EBmvKqq1SjDCMGb3PWQCmpY1qNqE0zz3CxVOmvKi3AK6wpQncJQiAA */
     context: {
-      basePath: "your source path here",
-      destinationPath: "your destination path here",
+      basePath: "your path here",
+      destinationPath: "your path here",
       directoriesToCheck: [],
       dirsToEvaluate: [],
       dirsToMove: [],
@@ -32,7 +32,7 @@ export const mediaScannerMachine = createMachine(
         invoke: {
           id: "scanLibrary",
           input: ({context: { basePath }}) => ({ basePath }),
-          src: fromPromise(({input}) => scanDirectories(input.basePath)),
+          src: fromPromise(async ({input}) => await scanDirectories(input.basePath)),
           onDone: [
             {
               target: "CheckingFilePermissions",
@@ -53,7 +53,7 @@ export const mediaScannerMachine = createMachine(
         invoke: {
           id: "checkFilePermissions",
           input: ({context: { directoriesToCheck }}) => ({ directoriesToCheck }),
-          src: fromPromise(({input: { directoriesToCheck }}) => checkFilePermissions(directoriesToCheck)),
+          src: fromPromise( async ({input: { directoriesToCheck }}) =>  await checkFilePermissions(directoriesToCheck)),
           onDone: [
             {
               target: "EvaluatingFiles",
@@ -97,7 +97,7 @@ export const mediaScannerMachine = createMachine(
         invoke: {
           id: "evaluatingFiles",
           input: ({context: { dirsToEvaluate, acceptedFileTypes }}) => ({ dirsToEvaluate, acceptedFileTypes}),
-          src: fromPromise(({input: { dirsToEvaluate, acceptedFileTypes }}) => evaluateFiles(dirsToEvaluate, acceptedFileTypes)),
+          src: fromPromise(async ({input: { dirsToEvaluate, acceptedFileTypes }}) => await evaluateFiles(dirsToEvaluate, acceptedFileTypes)),
           onDone: [
             {
               target: "MovingFiles",
@@ -115,7 +115,7 @@ export const mediaScannerMachine = createMachine(
         description: "Move all the files present in context to the destination library",
         invoke: {
           input: ({context: { dirsToMove, destinationPath }}) => ({ dirsToMove, destinationPath }),
-          src: fromPromise(({input: { dirsToMove, destinationPath }}) => moveFiles(dirsToMove, destinationPath)),
+          src: fromPromise(async ({input: { dirsToMove, destinationPath }}) => await moveFiles(dirsToMove, destinationPath)),
           id: "moveFiles",
 
           onError: [
